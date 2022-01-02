6db0c2cdeb16f3e80616f8c21d9801e015f19935
aloepker <91484770+aloepker@users.noreply.github.com>
2022-01-02 07:22:47 -0600

    finished ammending job class

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -13,7 +13,26 @@ namespace TechJobsOO
         public CoreCompetency JobCoreCompetency { get; set; }
 
         // TODO: Add the two necessary constructors.
+        public Job()
+        {
+            Id = nextId;
+            nextId++;
+        }
+        public Job(string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency) :this()
+        {
+            
+        }
 
+        public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
         // TODO: Generate Equals() and GetHashCode() methods.
     }
 }
